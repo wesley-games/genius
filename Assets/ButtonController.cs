088f4c9f105c9cd81b30e48fbe2274bc81d71c8b
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-03-21 14:47:35 -0300

    Creating board and buttons

---
+++ Assets/ButtonController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+    
+    public void Inflate()
+    {
+        transform.localScale += new Vector3(0.3f, 0.3f, 0);
+    }
+
+    public void Deflate()
+    {
+        transform.localScale -= new Vector3(0.3f, 0.3f, 0);
+    }
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int Level = 1;
+    public GameObject[] buttons;
+    
+    private bool lose = false;
+    private int sequenceSize = 3;
+    private GameObject[] sequence;
+
+    void Start()
+    {
+        StartCoroutine(GameLoop());
+    }
+
+    IEnumerator GameLoop () 
+    {
+        while (!lose)
+        {
+            sequence = GenerateRandomSequence(sequenceSize);
+            yield return StartCoroutine(AnimateButtons(sequence));
+            yield return new WaitForSeconds(2);
+        }
+        yield return null;
+    }
+
+    GameObject[] GenerateRandomSequence(int size)
+    {
+        GameObject[] sequence = new GameObject[size];
+        for (int i = 0; i < size; i++)
+        {
+            sequence[i] = buttons[Random.Range(0, 4)];
+        }
+        return sequence;
+    }
+
+    IEnumerator AnimateButtons(GameObject[] sequence) {
+        foreach (GameObject button in sequence)
+        {
+            button.GetComponent<ButtonController>().Inflate();
+            yield return new WaitForSeconds(1);
+            button.GetComponent<ButtonController>().Deflate();
+            yield return new WaitForSeconds(1);
+        }
+    }
+}
