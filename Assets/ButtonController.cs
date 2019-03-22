22b61e1a85168d4b1ee4b6bbbb7f3a0575b949c2
Wesley Rocha <wesley.rocha@hexagonagriculture.com>
2019-03-22 14:19:28 -0300

    Click on button

--- Assets/ButtonController.cs
+++ Assets/ButtonController.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class ButtonController : MonoBehaviour
 {
-    
+    public enum ButtonType {blue, red, green, yellow, pink};
+    public ButtonType type;
+
+    public delegate void ButtonClicked(ButtonController button);
+    public static event ButtonClicked OnButtonClicked;
+
     public void Inflate()
     {
         transform.localScale += new Vector3(0.3f, 0.3f, 0);
@@ -13,5 +18,10 @@ public class ButtonController : MonoBehaviour
     public void Deflate()
     {
         transform.localScale -= new Vector3(0.3f, 0.3f, 0);
+    }
+
+    void OnMouseDown()
+    {
+        OnButtonClicked(this);
     }
 }
--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -11,6 +11,16 @@ public class GameController : MonoBehaviour
     private int sequenceSize = 3;
     private GameObject[] sequence;
 
+    void OnEnable()
+    {
+        ButtonController.OnButtonClicked += OnButtonClicked;
+    }
+
+    void OnDisable()
+    {
+        ButtonController.OnButtonClicked -= OnButtonClicked;
+    }
+
     void Start()
     {
         StartCoroutine(GameLoop());
@@ -18,12 +28,12 @@ public class GameController : MonoBehaviour
 
     IEnumerator GameLoop () 
     {
-        while (!lose)
-        {
+        // while (!lose)
+        // {
             sequence = GenerateRandomSequence(sequenceSize);
             yield return StartCoroutine(AnimateButtons(sequence));
             yield return new WaitForSeconds(2);
-        }
+        // }
         yield return null;
     }
 
@@ -40,10 +50,16 @@ public class GameController : MonoBehaviour
     IEnumerator AnimateButtons(GameObject[] sequence) {
         foreach (GameObject button in sequence)
         {
+            Debug.Log(button.GetComponent<ButtonController>().type);
             button.GetComponent<ButtonController>().Inflate();
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.5f);
             button.GetComponent<ButtonController>().Deflate();
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(0.5f);
         }
+    }
+
+    void OnButtonClicked(ButtonController button)
+    {
+        Debug.Log(button.GetComponent<ButtonController>().type);
     }
 }
